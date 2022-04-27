a182c0e5d6e03c9d60e96635612842a94a950738
sagniknitr <basusagnik1995@gmail.com>
2022-04-27 04:00:13 +0000

    test for resnet-50

--- src/generate_json.py
+++ src/generate_json.py
@@ -4,8 +4,29 @@ from profiler import profile, ProfilerActivity
 from torch.autograd import kineto_available, _supported_activities, DeviceType
 from torch.autograd.profiler import record_function
 
+def get_params(model, recurse=False):
+    """Returns dictionary of paramters
+
+    Arguments:
+        model {torch.nn.Module} -- Network to extract the parameters from
+
+    Keyword Arguments:
+        recurse {bool} -- Whether to recurse through children modules
+
+    Returns:
+        Dict(str:numpy.ndarray) -- Dictionary of named parameters their
+                                   associated parameter arrays
+    """
+    params = {k: v.detach().gpu().numpy().copy()
+              for k, v in model.named_parameters(recurse=recurse)}
+    return params
+
+
 
 model = models.resnet18().cuda()
+for k, v in model.state_dict().items():
+    print(k)
+    print(type(v))
 inputs = torch.randn(5, 3, 224, 224).cuda()
 print(torch.autograd._supported_activities())
 with profile(activities=[
@@ -13,5 +34,5 @@ with profile(activities=[
     with record_function("model_inference"):
         model(inputs)
 
-prof.export_chrome_trace("trace.json")
+prof.export_chrome_trace("trace_resnet18.json")
 print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
