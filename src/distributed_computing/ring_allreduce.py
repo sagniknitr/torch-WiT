2d5da91a3893362e23e091587658630885483da8
Chameleon Cloud User <cc@sagnik.novalocal>
2022-05-01 00:10:44 +0000

    add profiling data for all-reduce

--- src/distributed_computing/ring_allreduce.py
+++ src/distributed_computing/ring_allreduce.py
@@ -1,72 +1,68 @@
-from multiprocessing import Process
-import argparse
+import os
 import torch
-import logging
+import torch.distributed as dist
+import torch.multiprocessing as mp
 import time
-import csv
 
-from torch import distributed as dist
-
-""" Implementation of a ring-reduce with addition (based on Pytorch example). """
 def allreduce(send, recv):
-       rank = dist.get_rank()
-       size = dist.get_world_size()
-       #send_buff = send.clone()
-       #recv_buff = send.clone()
-       accum = []
-       left = ((rank - 1) + size) % size
-       right = (rank + 1) % size
-       for i in range(size - 1):
-           if i % 2 == 0:
-               # Send send_buff
-               send_req = dist.isend(send, right)
-               dist.recv(recv, left)
-               accum[:] += recv[:]
-           else:
-               # Send recv_buff
-               send_req = dist.isend(recv, right)
-               dist.recv(send, left)
-               accum[:] += send[:]
-           send_req.wait()
-       recv[:] = accum[:]
+   rank = dist.get_rank()
+   size = dist.get_world_size()
+   send_buff = torch.zeros(send.size())
+   recv_buff = torch.zeros(send.size())
+   accum = torch.zeros(send.size())
+   accum[:] = send[:]
 
+   left = ((rank - 1) + size) % size
+   right = (rank + 1) % size
+
+   for i in range(size - 1):
+       if i % 2 == 0:
+           # Send send_buff
+           send_req = dist.isend(send_buff, right)
+           dist.recv(recv_buff, left)
+           accum[:] += recv[:]
+       else:
+           # Send recv_buff
+           send_req = dist.isend(recv_buff, right)
+           dist.recv(send_buff, left)
+           accum[:] += send[:]
+       send_req.wait()
+   recv[:] = accum[:]
 
 def run_allreduce(rank, sz):
 
-    for i in range(1, 100):
-        print("Iteration ", i) 
-        data = torch.rand(sz // 4, dtype=torch.float32)
-        recv = torch.zeros_like(data)
-        print("Original data", data)
-        print("starting all reduce")
-        st = time.time()
+    data = torch.rand(sz // 4, dtype=torch.float32)
+    recv = torch.zeros_like(data)
+    print("start profiling")
+    with torch.profiler.profile(
+        activities=[
+            torch.profiler.ProfilerActivity.CPU],
+        with_stack=False,
+        on_trace_ready=torch.profiler.tensorboard_trace_handler('./result_gloo'),
+        record_shapes=True
+    ) as p:
         allreduce(send=data, recv=recv)
-        en = time.time()
-        f = open('meas.csv', 'a+')
-        wrt = csv.writer(f)
-        rw = []
-        rw.append(en - st)
-        rw.append(sz)   
-        wrt.writerow(rw)
-        print(recv)
+        p.step()
 
-def init_process(rank, size, fn, world_size, backend='gloo'):
-   """ Initialize the distributed environment. """
-   print("Hello in init_process")
-   dist.init_process_group(backend="gloo", init_method="tcp://10.10.1.1:6588", rank=rank, world_size=world_size)
-   print("Init done")
-   fn(rank, size)
+    print(recv)
+
+
+def init_process(rank, size, fn, backend='gloo'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29600'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--master-ip", "-m", required=True, type=str)
-    parser.add_argument("--num-nodes", "-n", required=True, type=int)
-    parser.add_argument("--rank", "-r", required=True, type=int)
-    parser.add_argument("--size", "-s", required=True, type=int)
-    args = parser.parse_args()
-    init_process(
-                 rank=args.rank,
-                 size = args.size,
-                 fn = run_allreduce,
-                 world_size=args.num_nodes)
+    size = 4
+    processes = []
+    mp.set_start_method("spawn")
+    for rank in range(size):
+        p = mp.Process(target=init_process, args=(rank, size, run_allreduce))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
